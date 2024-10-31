3a305eeb1aff83de2d89833282b878efa597b303
alqasandyy123 <alqasandyyy212@gmail.com>
2024-10-31 23:41:58 +0800

    Add files via upload

--- Tugas 3/main.py
+++ Tugas 3/main.py
@@ -1,11 +1,27 @@
-# input user
+# variabel adalah tempat minyimpan data
 
-# data yang dimasukan
-data = input("masukan data : ")
+# menaruh data/assigment data
+a = 10
+x = 5
+panjang = 1000
 
-# cetak
-print("data =",data,"type :",type(data))
+# memanggil variabel
+print("nilai a :", a) 
+print("nilai x :, x")
+print("nilai panjang : ", panjang)
 
-# supaya tidak string lakukan casting
-angka = float(input("masukan angka: "))
-print("data :",angka,"type :",type(angka))
+#penamaan variabel
+nilai_y = 15 # dengan underscore
+juta10 = 100000000  # boleh dipakai
+nilaiangka = 100 # model camel
+
+print("nilai y :,y")
+
+
+
+# mengubah isi variabel
+print("nilai a:, a") # nilai a awal =10
+a=7
+print("nilai x :, x") # nilai a awal =7
+
+print("nilai x :, x") # nilai awal =5
