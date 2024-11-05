329d3336264d3dd8f44560d96f68bc028a4b980b
alqasandyy123 <alqasandyyy212@gmail.com>
2024-11-05 12:40:33 +0800

    Add files via upload

---
+++ polder alqa kls b/05-TPM02/LuasLingkaranmain.py
@@ -0,0 +1,6 @@
+# Mengonversi suhu dari Fahrenheit ke Celcius
+fahrenheit = float(input("Masukkan suhu dalam Fahrenheit: "))
+
+celcius = (fahrenheit - 32) * 5.0 / 9.0
+print(f"Suhu dalam Celcius: {celcius:.2f}")
+
