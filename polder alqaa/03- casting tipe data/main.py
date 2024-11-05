7f1f37f2f02bde4ec19dbafab5042ffce35f2dc2
alqasandyy123 <alqasandyyy212@gmail.com>
2024-11-05 12:43:41 +0800

    Add files via upload

---
+++ polder alqaa/03- casting tipe data/main.py
@@ -0,0 +1,30 @@
+# casting
+# mengubah tipe data ke tipe data lain
+# tipe data = int, str, bool
+
+## integer
+print("===INTEGER===")
+data_int = 9
+print("data a :", data_int, "type = ",type (data_int))
+
+# ubah integer ke float
+data_float =(data_int)
+# ubah integer ke string
+data_str = (data_int)
+# ubah integer ke boolean
+data_bool =  bool (data_int)
+# cetak
+print("data" ,data_float, "type," ,type (data_float))
+print("data" ,data_str, "type" ,type ,(data_str))
+print("data" , data_bool, "type" ,type ,(data_bool))
+
+## FLOAT
+print("===FLOAT===")
+data_float = 3.14
+print("data :", data_float,"tipe data",type(data_float))
+
+# ubah menjadi integer
+
+# ubah menjadi boolean
+
+# ubah menjadi sting
---
+++ polder alqaa/05-TPM02/BilanganGenapGanjilmain.py
@@ -0,0 +1,9 @@
+PI = 3.14159
+
+# Mengecek bilangan genap atau ganjil
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap.")
+else:
+    print(f"{angka} adalah bilangan ganjil.")
